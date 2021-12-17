02a87a292406c1bfd5db0a356b940d6b6b7572a7
Konstantin <zaydisk@yandex.ru>
2021-12-17 20:11:21 +0300

    Initial Commit

---
+++ VK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VK
+//
+//  Created by Konstantin Zaytcev on 17.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
